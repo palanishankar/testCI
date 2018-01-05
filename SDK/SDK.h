133359db3dca850f9b68f6ab5889c5c48ab32ca4
Palanisamy Sankar <palani.sankar@payrange.com>
2018-01-05 11:33:06 -0800

    sdk test commits

---
+++ SDK/SDK.h
@@ -0,0 +1,19 @@
+//
+//  SDK.h
+//  SDK
+//
+//  Created by Palanisamy Sankar on 1/5/18.
+//  Copyright © 2018 Palanisamy Sankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDK.
+FOUNDATION_EXPORT double SDKVersionNumber;
+
+//! Project version string for SDK.
+FOUNDATION_EXPORT const unsigned char SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDK/PublicHeader.h>
+
+
