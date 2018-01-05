bc6698d2e46c1e2447492df9460fef6dc448b300
Palanisamy Sankar <palani.sankar@payrange.com>
2018-01-05 11:20:21 -0800

    Initial Commit

---
+++ testCITests/testCITests.swift
@@ -0,0 +1,40 @@
+//
+//  testCITests.swift
+//  testCITests
+//
+//  Created by Palanisamy Sankar on 1/5/18.
+//  Copyright © 2018 Palanisamy Sankar. All rights reserved.
+//
+
+import XCTest
+@testable import testCI
+
+class testCITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testsam() {
+        print("hi")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
